bd65bd714eb76046322136d68e8c97f21c988abd
Matthias <48327140+Matthias1590@users.noreply.github.com>
2021-10-10 00:52:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name="consoledraw",
-    version="2.0.2",
+    version="2.2.0",
     description="A python module to update the console without flashing.",
     long_description=readme(),
     long_description_content_type="text/markdown",
