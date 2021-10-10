df0fd061e0739154773b133aed4e2ee891a04dfc
Matthias <48327140+Matthias1590@users.noreply.github.com>
2021-10-10 13:22:30 +0200

    Added comments and a function to generate the grid that uses caching

--- consoledraw/console.py
+++ consoledraw/console.py
@@ -1,16 +1,16 @@
 import os
 
-if os.name == 'nt':
+if os.name == "nt":
     import ctypes
     import msvcrt
 
     class _CursorInfo(ctypes.Structure):
-        _fields_ = [("size", ctypes.c_int),
-                    ("visible", ctypes.c_byte)]
+        _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
 
 class Console:
     def __init__(self, hideCursor: bool = True) -> None:
         self.text = ""
+        self.__gridCache = {}
 
         if hideCursor:
             if os.name == "nt":
@@ -24,11 +24,30 @@ class Console:
         
         self.update()
     
-    def __enter__(self, *args, **kwargs):
+    def __enter__(self, *args, **kwargs) -> None:
         self.clear()
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, *args, **kwargs) -> None:
         self.update()
+    
+    def __generateGrid() -> List[List[str]]:
+        # If we have already generated a grid with the current size of the terminal, just return a copy of that instead of generating it again
+        size = os.get_terminal_size()
+        if size in self.__gridCache:
+            return self.__gridCache[size].copy()
+
+        # Generate a grid the size of the terminal
+        grid = []
+        for _ in range(size.lines - 1):
+            row = []
+            for _ in range(size.columns):
+                row.append(" ")
+            grid.append(row)
+        
+        # Cache the grid so we don't have to generate it again later
+        self.__gridCache[size] = grid
+
+        return grid.copy()
 
     def write(self, text: str) -> None:
         "Writes to the console (use Console.print if you want this to behave like python's built-in print function)."
@@ -38,6 +57,7 @@ class Console:
     def print(self, *args, **kwargs) -> None:
         "Prints to the console (behaves like python's built-in print function)."
 
+        # Make sure to remove the file keyword argument if it exists, because we already set it ourselves
         if "file" in kwargs:
             kwargs.pop("file")
         print(*args, **kwargs, file=self)
@@ -54,25 +74,23 @@ class Console:
 
         self.text = self.text.rstrip()
 
-        grid = []
-        size = os.get_terminal_size()
-        for _ in range(size.lines - 1):
-            row = []
-            for _ in range(size.columns):
-                row.append(" ")
-            grid.append(row)
+        grid = self.__generateGrid()
 
         x, y = 0, 0
+        maxX = len(grid[0]) - 1
+        maxY = len(grid) - 1
         for char in self.text:
             if char == "\n":
                 x = 0
                 y += 1
             else:
-                if y >= len(grid) or x >= len(grid[0]):
+                if y > maxY or x > maxX:
                     raise ValueError("The console is too small to display the buffer.")
                 grid[y][x] = char
-                if (x := x + 1) == size.columns:
+                if x == maxX:
                     x = 0
                     y += 1
+                else:
+                    x += 1
 
-        print("".join(["".join(row) for row in grid]), end="")
+        print(end="".join(["".join(row) for row in grid]))
