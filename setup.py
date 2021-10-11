389433c7cc7ce28e57628204a482b112dd830b05
Matthias <48327140+Matthias1590@users.noreply.github.com>
2021-10-11 20:54:55 +0200

    Added py -m to pip commands

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="consoledraw",
-    version="2.3.11",
+    version="2.3.12",
     description="A python module to update the console without flashing.",
     long_description=readme(),
     long_description_content_type="text/markdown",
