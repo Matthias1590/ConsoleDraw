8bbfcac18926c81d760126206caa5bc0b7da6388
Matthias <48327140+Matthias1590@users.noreply.github.com>
2021-10-11 20:30:02 +0200

    Added python_requires keyword argument to setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="consoledraw",
-    version="2.3.6",
+    version="2.3.7",
     description="A python module to update the console without flashing.",
     long_description=readme(),
     long_description_content_type="text/markdown",
